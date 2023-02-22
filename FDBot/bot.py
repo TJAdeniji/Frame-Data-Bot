1f2a0933ecd79ca7f396f8bd624dcbb216fabc52
TJAdeniji <tjadeniji@gmail.com>
2023-02-21 21:23:57 -0500

    Local pip files generated, still not running

--- FDBot/bot.py
+++ FDBot/bot.py
@@ -9,7 +9,7 @@ load_dotenv()
 token = os.getenv("TOKEN")
 intents = discord.Intents.default()
 intents.message_content = True
-handler - logging .FileHandler(filename="discord.log", encoding="utf-8", mode='w')
+#handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode='w')
 
 client = discord.Client(intents=intents)
 
@@ -18,10 +18,11 @@ async def on_message(message):
         if message.author == client.user:
                 return
         if message.content.startswith("$hello"):
-                embed = discord.Embed(title = "Sample Embed", url = "https://dreamcancel.com/wiki/The_King_of_Fighters_XV/Kula_Diamond">
+                embed = discord.Embed(title = "Sample Embed", url = "https://dreamcancel.com/wiki/The_King_of_Fighters_XV/Kula_Diamond")
                 await message.channel.send(embed = embed)
 #    print(message.author.name + " said, " + message.content)
 #    if not message.author.id == "1072574558688788500":
 #            await client.send_message(message.channel, content = "GOT EM! TIME TO STYLE ON EM WITH A CLIMAX"
 
-client.run(token, logging_handler = handler)
+client.run(token)
+#client.run(token, log_handler = handler)
