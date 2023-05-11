cc49388721f88445f232808f54fa72cdad9436f9
TJAdeniji <tjadeniji@gmail.com>
2023-05-11 00:06:01 -0400

    Update bot.py
    Latest Update, pending functioning web scraper.

--- FDBot/bot.py
+++ FDBot/bot.py
@@ -1,5 +1,8 @@
 import discord 
+from discord import app_commands
 from discord.ext import commands
+from typing import Literal, Optional
+from discord.ext.commands import Greedy, Context
 
 import logging
 import requests 
@@ -8,86 +11,152 @@ from bs4 import BeautifulSoup
 import os 
 from dotenv import load_dotenv 
 
+import aiohttp
+import asyncio
 
 load_dotenv()
 
 token = os.getenv("TOKEN")
-intents = discord.Intents.default()
+intents = discord.Intents.all()
 intents.message_content = True
 handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode='w')
 
-bot = commands.Bot(command_prefix="?", intents=intents)
+bot = commands.Bot(command_prefix="!", intents=intents)
 
-#
-# 
+selected_char = ""
+page_char = ""
 
-#3/1 - command Method to get the char's name, then the move or special in another method?
-# all moves follow a name_move convention in the table, might be an easy way to locate them when scraping. 
-
-#3/3 text-align: center is the location for all the rows with the data for the move list
-#might hard code the labels, at least for now for simplicity's sake.
-
-# @bot.command()
-# async def char(ctx, *, arg):
-
-#         await ctx.send(arg)
-
-
-
-#
-#Bot not syncing commands to server.  
-#
+roster = {
+     "shunei": "Shun'ei",
+     "meitenkun": "Meitenkun", 
+     "benimaru": "Benimaru_Nikaido",
+     "ash": "Ash_Crimson",
+     "elisabeth": "Elisabeth_Blanctorche",
+     "kukri": "Kukri",
+     "kyo": "Kyo_Kusanagi",
+     "iori": "Iori_Yagami",
+     "chizuru": "Chizuru_Kagura",
+     "k": "K'",
+     "maxima": "Maxima",
+     "whip": "Whip",
+     "isla": "Isla",
+     "heidern": "Heidern",
+     "dolores": "Dolores",
+     "terry": "Terry_Bogard",
+     "andy": "Andy_Bogard",
+     "joe": "Joe_Higashi",
+     "ryo": "Ryo_Sakazaki",
+     "robert": "Robert_Garcia",
+     "king": "King",
+     "yashiro": "Yashiro_Nanakase",
+     "shermie": "Shermie",
+     "chris": "Chris",
+     "athena": "Athena_Asamiya",
+     "mai": "Mai_Shiranui",
+     "yuri": "Yuri_Sakazaki",
+     "leona": "Leona_Heidern",
+     "ralf": "Ralf_Jones",
+     "clark": "Clark_Still",
+     "antonov": "Antonov",
+     "ramon": "Ramón",
+     "kod": "King_of_Dinosaurs",
+     "krohnen": "Krohnen",
+     "kula": "Kula_Diamond",
+     "angel": "Ángel",
+     "bluemary": "Blue_Mary",
+     "vanessa": "Vanessa",
+     "luong": "Luong",
+     "rock": "Rock_Howard",
+     "bjenet": "B.Jenet",
+     "gato": "Gato",
+     "geese": "Geese_Howard",
+     "billy": "Billy_Kane",
+     "yamazaki": "Ryuji_Yamazaki",
+     "oyashiro": "Orochi_Yashiro",
+     "oshermie": "Orochi_Shermie",
+     "ochris": "Orochi_Chris",
+     "haohmaru": "Haohmaru",
+     "nakoruru": "Nakoruru",
+     "darli": "Darli_Dagger",
+     "orugal": "Omega_Rugal",
+     "shingo": "Shingo_Yabuki",    
+     "kim": "Kim_Kaphwan"
+}
 
 
 @bot.event
 async def on_ready():
         print(f'Logged in as {bot.user} (ID: {bot.user.id})')
-        print('------')
-
-# @bot.command(name="sync", description="Owner only")
-# async def sync(interaction: discord.Interaction):
-#         if interaction.user.id == 213820249420595200:
-#                 await bot.tree.sync()
-#                 print('Command tree synced.')
-#         else:
-#                 await interaction.response.send_message('You must be the owner to use this command!')
-
-@bot.event
-async def on_message(message):
-        if message.user.id == 213820249420595200 and message.content.startsWith("$sync"):
-                bot.tree.sync()
-                await bot.process_commands(message)
-                print('Command tree synced.')
+        print('------')              
+             
+@bot.command()
+@commands.guild_only()
+@commands.is_owner()
+async def sync(
+  ctx: Context, guilds: Greedy[discord.Object], spec: Optional[Literal["~", "*", "^"]] = None) -> None:
+    if not guilds:
+        if spec == "~":
+            synced = await ctx.bot.tree.sync(guild=ctx.guild)
+        elif spec == "*":
+            ctx.bot.tree.copy_global_to(guild=ctx.guild)
+            synced = await ctx.bot.tree.sync(guild=ctx.guild)
+        elif spec == "^":
+            ctx.bot.tree.clear_commands(guild=ctx.guild)
+            await ctx.bot.tree.sync(guild=ctx.guild)
+            synced = []
         else:
-                await message.response.send_message('You must be the owner to use this command!')
-         
+            synced = await ctx.bot.tree.sync()
+
+        await ctx.send(
+            f"Synced {len(synced)} commands {'globally' if spec is None else 'to the current guild.'}"
+        )
+        return
+
+    ret = 0
+    for guild in guilds:
+        try:
+            await ctx.bot.tree.sync(guild=guild)
+        except discord.HTTPException:
+            pass
+        else:
+            ret += 1
+
+    await ctx.send(f"Synced the tree to {ret}/{len(guilds)}.") 
+
 @bot.tree.command(name="char", description="select a character")
-async def char(interaction: discord.Interaction):
-        pass
+async def char(interaction: discord.Interaction, selected_char: str):
         
-@bot.event
-async def on_message(message):
-        if message.author == bot.user:
-                return
+        if selected_char in roster:
+            page_char = roster.get(selected_char)
+        else:
+            page_char = selected_char
+            
+        URL = (f"https://dreamcancel.com/wiki/The_King_of_Fighters_XV/{page_char}/Data")
+        async with aiohttp.ClientSession() as session:
+             async with session.get(URL) as resp:
+                  print(resp.status)
+                  data = await resp.text()
+        soup = BeautifulSoup(data, 'html5lib')
+        print(soup.prettify())
+        raw_moveList = soup.find_all("table", "wikitable")
         
-        if message.content.startswith("$hello"):
-                URL = "https://dreamcancel.com/wiki/The_King_of_Fighters_XV/"
-                r = requests.get(URL)
-                soup = BeautifulSoup(r.content, 'html5lib')
-                #moveList = r.find_all("div", class_ = "float:right")
-                #r = requests.get(URL + charName + "/Data"
-                embed = discord.Embed(title = "Sample Embed", url = "https://dreamcancel.com/wiki/The_King_of_Fighters_XV/")
-                embed.set_thumbnail(url = "")
-                embed.add_field(name="Damage", value="Damage", inline="True")
-                embed.add_field(name="Guard", value="Guard", inline="True")
-                embed.add_field(name="Cancel", value="Cancel", inline="True")
-                embed.add_field(name="Startup", value="Startup", inline="True")
-                embed.add_field(name="Recovery", value="Recovery", inline="True")
-                embed.add_field(name="Hit Advantage", value="Hit Advantage", inline="True")
-                embed.add_field(name="Block Advantage", value="Block Advantage", inline="True")
-                embed.add_field(name="Invulnerability", value="Invulnerability", inline="True")
-                embed.add_field(name="Stun", value="Stun", inline="True")
-                embed.add_field(name="Guard Damage", value="Guard Damage", inline="True")
-                await message.channel.send(embed = embed)
+        
+        #moveList = r.find_all("div", class_ = "float:right")
+        #r = requests.get(URL + charName + "/Data"
+        charEmbed = discord.Embed(title = "Sample Embed", url = URL)
+        charEmbed.set_thumbnail(url = "")
+        charEmbed.add_field(name="Damage", value="Damage", inline="True")
+        charEmbed.add_field(name="Guard", value="Guard", inline="True")
+        charEmbed.add_field(name="Cancel", value="Cancel", inline="True")
+        charEmbed.add_field(name="Startup", value="Startup", inline="True")
+        charEmbed.add_field(name="Recovery", value="Recovery", inline="True")
+        charEmbed.add_field(name="Hit Advantage", value="Hit Advantage", inline="True")
+        charEmbed.add_field(name="Block Advantage", value="Block Advantage", inline="True")
+        charEmbed.add_field(name="Invulnerability", value="Invulnerability", inline="True")
+        charEmbed.add_field(name="Stun", value="Stun", inline="True")
+        charEmbed.add_field(name="Guard Damage", value="Guard Damage", inline="True")
+        await interaction.response.defer()
+        await interaction.followup.send(embed = charEmbed)
+
 
 bot.run(token, log_handler = handler)
